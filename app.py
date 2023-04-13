30dfda392a5cccb0f574d16470c63322ace5e203
Raj Thakare <87536278+rajthakare237@users.noreply.github.com>
2023-04-13 20:00:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import pickle
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer()
+
+df = pd.read_csv('bank.csv')
+
+tfidf.fit_transform(df['text']).toarray()
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    text = request.form.get('text')
+
+    # 2. vectorize
+    vector_input = tfidf.transform([text]).toarray()
+    # 3. predict
+    result = model.predict(vector_input)[0]
+
+    return jsonify({'target': str(result)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
